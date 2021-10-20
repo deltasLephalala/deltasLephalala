cd452c1584232be2eab57ab18f930a71d5342f1a
Deltas Lephalala <dlephalala@aolc.co.za>
2021-10-20 09:54:35 +0200

    first commit

---
+++ AOLC_WebApplication/Data/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOLCWebApplication.Data
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        //public byte[] Image { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+       // public string Designation { get; set; }
+       // public string Gender { get; set; }
+        //public DateTime Dob { get; set; }
+        public string Phone { get; set; }
+        public string Address1 { get; set; }
+        //public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ AOLC_WebApplication/Data/ClientService.cs
@@ -0,0 +1,66 @@
+﻿using AOLC_WebApplication.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOLCWebApplication.Data
+{
+    public class ClientService
+    {
+        #region Property
+        private readonly ApplicationDbContext _appDBContext;
+        #endregion
+
+        #region Constructor
+        public ClientService(ApplicationDbContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        #endregion
+
+        #region Get List of Clients
+        public async Task<List<Client>> GetAllClientsAsync()
+        {
+            return await _appDBContext.Clients.ToListAsync();
+        }
+        #endregion
+
+        #region Insert Client
+        public async Task<bool> InsertClientAsync(Client client)
+        {
+            await _appDBContext.Clients.AddAsync(client);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Get Client by Id
+        public async Task<Client> GetClientAsync(int Id)
+        {
+            Client client = await _appDBContext.Clients.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            return client;
+        }
+        #endregion
+
+        #region Update Client
+        public async Task<bool> UpdateClientAsync(Client client)
+        {
+            _appDBContext.Clients.Update(client);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region DeleteClient
+        public async Task<bool> DeleteClientAsync(Client client)
+        {
+            _appDBContext.Remove(client);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+    }
+}
+
